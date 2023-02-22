538e274333032c9063b77d34f7beb0d0c3bf7dcc
Kai Eugene <kai.yougene@gmail.com>
2023-02-22 08:43:55 -0800

    a more somewhat working calculator

--- app.js
+++ app.js
@@ -1,105 +1,114 @@
+class Calculator {
+    constructor(topInnerText, bottomInnerText){
+        this.topInnerText = topInnerText;
+        this.bottomInnerText = bottomInnerText;
+        this.clear();
+    }
+
+    clear() {
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operation = undefined;
+    }
+
+    allClear() {
+
+    }
+
+    appendNumber(number) {
+        if (number === '.' && this.currentOperand.includes('.')) return;
+        this.currentOperand = this.currentOperand.toString() + number.toString();
+    }
+
+    chooseOperation(operation) {
+        if (this.currentOperand === '') return;
+        if (this.previousOperand !== ''){
+            this.compute();
+        }
+        this.operation = operation; 
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
+    }
+
+    compute() {
+        let computation;
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if (isNaN(prev)|| isNaN(current)) return
+        switch (this.operation) {
+            case '+':
+                computation = prev + current;
+                break
+            case '-':
+                computation = prev - current;
+                break
+            case '*':
+                computation = prev * current;
+                break;
+            case '÷':
+                computation = prev / current;
+            default:
+                return;
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = '';
+    }
+
+
+    updateDisplay() {
+        this.bottomInnerText.innerText = this.currentOperand;
+        this.topInnerText.innerText = this.previousOperand;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const numberButtons = document.querySelectorAll('.number');
+const operationButtons = document.querySelectorAll('.operation');
+const equalsButton = document.querySelector('.equal');
 const allClearButton = document.querySelector('.allclear');
 const clearButton = document.querySelector('.clear');
-const nine = document.querySelector('.nine');
-const eight = document.querySelector('.eight');
-const seven = document.querySelector('.seven');
-const six = document.querySelector('.six');
-const five = document.querySelector('.five');
-const four = document.querySelector('.four');
-const three = document.querySelector('.three');
-const two = document.querySelector('.two');
-const one = document.querySelector('.one');
-const zero = document.querySelector('.zero')
-const divide = document.querySelector('.divide');
-const multiply = document.querySelector('.multiply');
-const plus = document.querySelector('.plus');
-const subtract = document.querySelector('.subtract');
-const decimal = document.querySelector('.decimal');
-const equal = document.querySelector('.equal');
-const topScreen = document.querySelector('.top');
-const bottomScreen = document.querySelector('.bottom');
-let bottomValues = [];
-let topValues = [];
-let operator;
+const topInnerText = document.querySelector('.top');
+const bottomInnerText = document.querySelector('.bottom');
 
-//when a button is clicked, insert that button into the bottomScreen div
- updateBottom = (value) => {
-    if (bottomValues.length < 9){
-        bottomValues.push(value);
-        bottomScreen.innerText = `${bottomValues.join('')}`
-    }
-}
+const calculator = new Calculator(topInnerText, bottomInnerText);
 
-updateTop = (value) => {
-    topScreen.innerText = `${topValues.join('')}` + value
-}
+numberButtons.forEach(button => {
+    button.addEventListener('click', () =>{
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    });
+});
 
+operationButtons.forEach(button => {
+    button.addEventListener('click', () =>{
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
+    });
+});
 
-//number buttons
-nine.onclick = () => updateBottom(9);
-eight.onclick = () => updateBottom(8);
-seven.onclick = () => updateBottom(7);
-six.onclick = () => updateBottom(6);
-five.onclick = () => updateBottom(5);
-four.onclick = () => updateBottom(4);
-three.onclick = () => updateBottom(3);
-two.onclick = () => updateBottom(2);
-one.onclick = () => updateBottom(1);
-zero.onclick = () => updateBottom(0);
-decimal.onclick = () => {
-    if (!(bottomValues.includes('.'))){
-        updateBottom('.');
-    }
-}
+equalsButton.addEventListener('click', button=> {
+    calculator.compute();
+    calculator.updateDisplay();
+})
 
-//operand buttons
+allClearButton.addEventListener('click', button=> {
+    calculator.clear();
+    calculator.updateDisplay();
+})
 
-plus.onclick = () => {
-    if (!(bottomValues.includes('+') && bottomValues[0] !== '+')){
-        topValues = bottomValues;
-        updateTop('+');
-        operator = '+';
-    }
-    bottomValues = [];
-    updateBottom();
-}
-
-subtract.onclick = () => {
-    if (!(bottomValues.includes('-') && bottomValues[0] !== '-')){
-        topValues = bottomValues;
-        updateTop('-')
-    }
-    bottomValues = [];
-    updateBottom();
-}
-
-divide.onclick = () => {
-    if (!(bottomValues.includes('÷') && bottomValues[0] !== '÷')){
-        topValues = bottomValues;
-        updateTop('÷');
-    }
-    bottomValues = [];
-    updateBottom();
-}
-
-multiply.onclick = () => {
-    if (!(bottomValues.includes('*') && bottomValues[0] !== '*')){
-        topValues = bottomValues;
-        updateTop('*');
-    }
-    bottomValues = [];
-    updateBottom();
-}
-
-//equal button
-evaluate = () => {
-    let a = Number(topValues.join(''));
-
-    let b = Number(bottomValues.join(''));
-
-    let result = a/b;
-   
-    bottomScreen.innerText = `${result}`
-}
-
-equal.onclick = () => evaluate();
